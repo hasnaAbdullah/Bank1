6dfc63e6c32297b93ddcb5e875dde1919917b393
hasna.ak <hasna.ak@MEML2030>
2026-01-06 21:56:20 -0800

    22

--- Program.cs
+++ Program.cs
@@ -6,41 +6,109 @@
         {
 
 
-            int accNumber = 1234;     
-            double balance = 500;     
-            double fees = 5;            
-
-            Console.Write("Enter account number: ");
-            int enteredAcc = int.Parse(Console.ReadLine());
-
-            Console.Write("Enter amount to withdraw: ");
-            double amount = double.Parse(Console.ReadLine());
-
-           
-            if (enteredAcc == accNumber)
             {
-                
-                if (balance >= amount + fees)
+                static void Main()
                 {
-                    balance = balance - (amount + fees);
 
-                    Console.WriteLine("Withdraw successful");
-                    Console.WriteLine("Amount withdrawn: " + amount);
-                    Console.WriteLine("Fees: " + fees);
-                    Console.WriteLine("Remaining balance: " + balance);
+                    int acc1 = 1111;
+                    int acc2 = 2222;
+
+                    double balance1 = 1000;
+                    double balance2 = 500;
+                    double fees = 10;
+
+                    Console.WriteLine("Welcome to the Bank");
+                    Console.WriteLine("1 - Withdraw");
+                    Console.WriteLine("2 - Transfer");
+                    Console.Write("Choose service: ");
+
+                    int choice = int.Parse(Console.ReadLine());
+
+                    switch (choice)
+                    {
+
+                        case 1:
+                            Console.Write("Enter account number: ");
+                            int acc = int.Parse(Console.ReadLine());
+
+                            Console.Write("Enter amount: ");
+                            double amount = double.Parse(Console.ReadLine());
+
+                            if (acc == acc1)
+                            {
+                                if (balance1 >= amount + fees)
+                                {
+                                    balance1 -= (amount + fees);
+
+                                    Console.WriteLine("Withdraw successful");
+                                    Console.WriteLine("Remaining balance: " + balance1);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Insufficient balance");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Account not found");
+                            }
+                            break;
+
+
+                        case 2:
+                            Console.Write("Enter sender account number: ");
+                            int sender = int.Parse(Console.ReadLine());
+
+                            Console.Write("Enter receiver account number: ");
+                            int receiver = int.Parse(Console.ReadLine());
+
+                            Console.Write("Enter amount: ");
+                            double transferAmount = double.Parse(Console.ReadLine());
+
+                            if (sender == acc1 && receiver == acc2)
+                            {
+                                if (balance1 >= transferAmount + fees)
+                                {
+                                    balance1 -= (transferAmount + fees);
+                                    balance2 += transferAmount;
+
+                                    Console.WriteLine("Transfer successful");
+                                    Console.WriteLine("Sender balance: " + balance1);
+                                    Console.WriteLine("Receiver balance: " + balance2);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Insufficient sender balance");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Account not found");
+                            }
+                            break;
+
+                        default:
+                            Console.WriteLine("Invalid choice");
+                            
+
+                            break;
+                            Console.ReadLine();
+                            Console.WriteLine("Press Enter to exit...");
+                            Console.ReadLine();
+
+
+
+
+                    }
                 }
-                else
-                {
-                    Console.WriteLine("Sorry, insufficient balance");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Account not found");
             }
         }
     }
-
-
 }
-  
+   
+
+
+
+    
+
+
